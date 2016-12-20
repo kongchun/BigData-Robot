159dacb22db261a538713eb1e3ac7051dc7b1270
xiaoyaomaopp <xiaoyaomaopp@sohuc.om>
2016-12-20 15:01:22 +0800

    添加调用python脚本

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var Robot = require('./robot.js');
 var cronJob = require("cron").CronJob;
-
+var  spawn = require( 'child_process').spawn;
 
 new cronJob('1 0 0 * * *', function() {
-	Robot.updateArticle()
+	Robot.updateArticle().then(function(){
+		spawn("python",["E:/git_pro/BigData-ML/MLmain.py"],{})
+	})
 }, null, true, 'Asia/Shanghai');
 Robot.updateArticle()
