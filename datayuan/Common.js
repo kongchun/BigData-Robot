0ecd8d6bee83137aca6aef218dde255f3e81bef2
lindandan <969990114@qq.com>
2017-02-09 14:08:06 +0800

    添加datayuan数据源的爬虫程序;添加依赖到package.json文件

---
+++ datayuan/Common.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Administrator on 2016/12/21.
+ */
+var Common = {
+    getAbstract:function(arr,$){
+        var abstracts = [];
+        for(var i = 0;i < arr.length;i++){
+            var abstract = $(arr[i]).text();
+            abstracts.push(abstract);
+        }
+        return abstracts;
+    },
+    getURLNumber:function(url){
+        if(url){
+            var lIndex = url.lastIndexOf("/");
+            var dotIndex = url.lastIndexOf(".");
+            return parseInt(url.substring(lIndex+1,dotIndex));
+        }
+    },
+    formatDate:function(str){
+        str = str.replace(/-/g,"/");
+        return new Date(str);
+    },
+    checkThumbnail:function(str){
+        if(str){
+            return "http://www.datayuan.cn"+str;
+        }else{
+            return "";
+        }
+    },
+    checkTitle:function(container){
+        var newTitle = container.find("h2").text();
+        var titleStr = container.find("h2").html();
+        if(titleStr.length>100){
+             var firstIndex = titleStr.indexOf("<div");
+             newTitle = titleStr.substr(0,firstIndex);
+        }
+        return newTitle;
+    },
+	formatHtml:function(html){
+	   var newHtml = html.replace(/src="\/u\/cms/g,'src="http://www.datayuan.cn/u/cms');
+	   return newHtml; 
+	}
+}
+module.exports = Common;
---
+++ datayuan/update/ArticleDetail.js
@@ -0,0 +1,30 @@
+var request = require("request");
+var cheerio = require("cheerio");
+var Common = require("../Common.js");
+var loader = require("../iRobots/loader.js");
+var ArticleDetail = {
+    getAticleDetail:function(url){
+        var $this = this;
+        return loader.getDOM(url).then(function($){
+            return $this.parseHtml($);
+        }).catch(function(e){
+            console.log("catch 异常");
+            console.log(e);
+        });
+    },
+    parseHtml:function($){
+        var container = $(".wz-div");
+        var daodu = container.find(".daodu p").text();
+        var html = container.find("#divcontent").html();
+        var content = container.find("#divcontent").text();
+        var detail = {
+            daodu:daodu,
+            html:Common.formatHtml(html),
+            content:content.replace(/[\r\n]/ig, "")
+        }
+        return detail;
+    }
+}
+module.exports = ArticleDetail;
+
+
